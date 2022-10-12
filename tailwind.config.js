34347b44ca90782cf7fb15b9f43f6beb9183d4b2
Mikołaj Lewandowski <mikdyb@gmail.com>
2022-10-12 22:40:34 +0200

    style: add custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      display: "'Press Start 2P', sans-serif",
+    },
     extend: {},
   },
   plugins: [],
