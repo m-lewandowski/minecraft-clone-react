54a1a9e082990245f49bfb84069bf39c0fb52390
Mikołaj Lewandowski <mikdyb@gmail.com>
2022-10-12 22:00:03 +0200

    feat: add Ground component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Canvas } from "@react-three/fiber";
-
+import { Physics } from "@react-three/cannon";
 import Sky from "components/Sky";
+import Ground from "components/Ground";
 
 const App = () => {
   return (
     <div className="h-screen select-none">
       <Canvas>
         <Sky />
+        <Physics>
+          <Ground />
+        </Physics>
       </Canvas>
     </div>
   );
